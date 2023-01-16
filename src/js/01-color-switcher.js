7d83155a10afb1aec236441e2145dedc6d49faab
Sayuk <sayukbohdan@gmail.com>
2023-01-16 21:22:46 +0200

    first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startFon = document.querySelector('.start');
+const stopFon = document.querySelector('.stop');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let changeFon = [];
+
+startFon.addEventListener('click', () => {
+    changeFon = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+    }, 1000);
+    startFon.disabled = true;
+     stopFon.disabled = false;
+});
+
+
+stopFon.addEventListener('click', () => {
+    clearInterval(changeFon);
+     startFon.disabled = false;
+     stopFon.disabled = true;
+});
+
+
+
+
 
