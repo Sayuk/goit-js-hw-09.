565b1bb4d20ce56ac3dedabe9b3b0101b0021d8d
Sayuk <sayukbohdan@gmail.com>
2023-01-19 23:16:20 +0200

    e

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -27,21 +27,14 @@ delay += step;
 }
 }
 
-
-
-
-
-
-
-
-
-
-
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  let promiseValue = {position, delay};
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(promiseValue);
+    }
+      reject(promiseValue);
+  })
 }
